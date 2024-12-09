c8b2239706c0b3211db3202a99b40d37083eb79f
dominik <papesd@proton.me>
2024-12-09 01:24:29 +0100

    final

--- site/scripts/load_pics.js
+++ site/scripts/load_pics.js
@@ -1,7 +1,5 @@
-// Select the gallery container
 const gallery = document.getElementById('gallery');
 
-// Generate image file names and add to the gallery
 for (let i = 1; i <= 50; i++) {
     const img = document.createElement('img');
     img.src = `images/img${i}.jpg`;
